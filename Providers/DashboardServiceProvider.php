95d1d05e3174d5509264c148e10210fc8ef46a8c
Juan Escalante <jcec007@gmail.com>
2020-12-11 10:41:35 -0500

    Add: setting-fields config in the Service Provider

--- Providers/DashboardServiceProvider.php
+++ Providers/DashboardServiceProvider.php
@@ -66,6 +66,7 @@ class DashboardServiceProvider extends ServiceProvider
         $this->publishConfig('dashboard', 'permissions');
         $this->publishConfig('dashboard', 'config');
         $this->publishConfig('dashboard', 'settings');
+        $this->publishConfig('dashboard', 'settings-fields');
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
     }
 
